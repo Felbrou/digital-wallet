a5b7f115442d147763a502b06c47769669d016b6
Felbrou <15351171+Felbrou@users.noreply.github.com>
2022-04-05 19:03:32 -0300

    add column password is user table

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,5 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.describe User do
-  describe 'validations' do
-    it 'valida presença do nome' do
-      is_expected.to validate_presence_of(:name)
-    end
-    it 'valida presença do nome' do
-      is_expected.to validate_presence_of(:name)
-    end
-  end
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
