442aed2ad492f722e96e88d96455c69fda2dfd00
Felbrou <15351171+Felbrou@users.noreply.github.com>
2022-04-15 21:26:04 -0300

    update shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,12 @@
 
 require 'rails_helper'
   
-RSpec.describe User do  # method rspec for User class models/concerns/user.rb
+RSpec.describe User, type: :model do  # method rspec for User class models/concerns/user.rb
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { should have_many :orders }
   end
 end
   
