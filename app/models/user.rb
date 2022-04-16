6b1988aa79d2fdbe0db4a16b478cf6d4e0869a42
Felbrou <15351171+Felbrou@users.noreply.github.com>
2022-04-16 12:13:48 -0300

    update shoulda test in user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 # :nodoc:
-class User < ApplicationRecord
+class User < ApplicationRecord; ActiveModel::Model
+  attr_accessor :password, :name, :email
   VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   has_secure_password
@@ -10,4 +11,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 260}, format: { with:VALID_EMAIL_FORMAT }, 
   uniqueness: {case_sensitive: false}
   before_save { self.email = email.downcase }
+  validates_length_of :password, minimum: 6
+  validates_length_of :name, maximum: 50 
+  validates_length_of :email, maximum: 260
 end
